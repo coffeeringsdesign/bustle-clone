ec9b21ae34a734da4ad49b4a996f5a73c61cffc7
Kendra <kendra@coffeeringsdesign.com>
2018-11-01 12:51:50 -0700

    get the large article component finished, begin small article component

--- src/app/small-article/small-article.component.ts
+++ src/app/small-article/small-article.component.ts
@@ -1,22 +1,28 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Article } from '../models/article.model';
+import { ArticleService } from '../article.service';
+import { Router } from '@angular/router';
+import { FirebaseListObservable } from 'angularfire2/database';
+
 
 @Component({
   selector: 'app-small-article',
   templateUrl: './small-article.component.html',
-  styleUrls: ['./small-article.component.css']
+  styleUrls: ['./small-article.component.css'],
+  providers: [ArticleService]
 })
 export class SmallArticleComponent implements OnInit {
-    @Input() childArticleList: Article[];
-    @Output() editClickSender = new EventEmitter();
+  articles: FirebaseListObservable<any[]>;
+  currentRoute: string = this.router.url;
 
-    editButtonClicked(articleToEdit: Article) {
-      this.editClickSender.emit(articleToEdit);
-    }
-
-  constructor() { }
+  constructor(private articleService: ArticleService, private router: Router) { }
 
   ngOnInit() {
+    this.articles = this.articleService.getArticles();
   }
 
+  goToDetailPage(clickedArticle) {
+    this.router.navigate(['articles', clickedArticle.$key]);
+    // console.log(clickedArticle.$key);
+  }
 }
